cf2f6e7315d1cd78079186313c092727e59cf36e
Excalibur0w0 <752408303@qq.com>
2019-01-13 21:48:18 +0800

    update func use

--- for/for.go
+++ for/for.go
@@ -1,15 +1,21 @@
 package main
 
 func main() {
+	// 正常的循环
 	// for i := 0; i < 10; i++ {
 	// 	print(i)
 	// }
 
-	// 单个表达式的赋值
+	// for循环单个表达式，类似while
 	a := 1
 	b := 3
 	for a < b {
 		a++
 		print(a)
 	}
+
+	// // 无限循环
+	// for true {
+
+	// }
 }
---
+++ func/func.go
@@ -0,0 +1,9 @@
+package main
+
+func add(a, b float32) float32 {
+	return a + b
+}
+
+func main() {
+	print(add(3, 1))
+}
